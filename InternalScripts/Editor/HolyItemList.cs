404ecb3636e18db242781a8dd9f1469c12d67190
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2026-08-27 22:34:07 +0300

    Build compatible

--- InternalScripts/Editor/HolyItemList.cs
+++ InternalScripts/Editor/HolyItemList.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System;
--- Samples~/PublicScripts/Editor/HolyItemEditor.cs
+++ Samples~/PublicScripts/Editor/HolyItemEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -86,3 +87,4 @@ namespace Holylib.ItemEditor
         }
     }
 }
+#endif
